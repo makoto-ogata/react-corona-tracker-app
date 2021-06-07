db1049518a27d022fd1840db2fd30161960645ab
Makoto Ogata <ogatamakoto1976@gmail.com>
2021-06-08 00:51:53 +0900

    Create a function to display data by country

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,13 +1,17 @@
 import Title from '../components/Title';
 import Selector from '../components/Selector';
+import Results from '../components/Results';
 
-const TopPage = () => {;
+const TopPage = (props) => {
   return (
-    <div>
-      <Title />
-      <Selector />
+    <div className="top-page-container">
+      <div>
+        <Title />
+        <Selector countriesJson={props.countriesJson} setCountry={props.setCountry} getCountryData={props.getCountryData} />
+        <Results countryData={props.countryData} />
+      </div>
     </div>
   );
-}
+};
 
 export default TopPage;
