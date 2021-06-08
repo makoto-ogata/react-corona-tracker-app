2598d0da27d351a6df5580c603f533829617632c
Makoto Ogata <ogatamakoto1976@gmail.com>
2021-06-09 00:36:33 +0900

    add useEffect

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 
 function App() {
   const [loading, setLoading] = useState(false);
-	const [country, setCountry] = useState("");
+	const [country, setCountry] = useState("sweden");
 	const [countryData, setCountryData] = useState({
 		date: "",
 		newConfirmed: "",
@@ -16,24 +16,29 @@ function App() {
 		totalRecovered: "",
 	});
   const [allCountriesData, setAllCountriesData] = useState([]);
-	const getCountryData = () => {
-    setLoading(true);
-		fetch(`https://api.covid19api.com/country/${country}`)
-			.then((res) => res.json())
-			.then((data) => {
-				setCountryData({
-					date: data[data.length - 1].Date,
-					newConfirmed:
-						data[data.length - 1].Confirmed - data[data.length - 2].Confirmed,
-					totalConfirmed: data[data.length - 1].Confirmed,
-					newRecovered:
-						data[data.length - 1].Recovered - data[data.length - 2].Recovered,
-					totalRecovered: data[data.length - 1].Recovered,
-				});
-        setLoading(false);
-			})
-      .catch(err => alert('エラーが発生しました。ページをリロードして、もう一度トライするか、国を選択してください。'));
-	}
+	
+
+  useEffect(()=>{
+    const getCountryData = () => {
+        setLoading(true);
+        fetch(`https://api.covid19api.com/country/${country}`)
+          .then((res) => res.json())
+          .then((data) => {
+            setCountryData({
+              date: data[data.length - 1].Date,
+              newConfirmed:
+                data[data.length - 1].Confirmed - data[data.length - 2].Confirmed,
+              totalConfirmed: data[data.length - 1].Confirmed,
+              newRecovered:
+                data[data.length - 1].Recovered - data[data.length - 2].Recovered,
+              totalRecovered: data[data.length - 1].Recovered,
+            });
+            setLoading(false);
+          })
+          .catch(err => alert('エラーが発生しました。ページをリロードして、もう一度トライするか、国を選択してください。'));
+      }
+      getCountryData();
+  }, [country])
 
   useEffect(() => {
     console.log('test')
@@ -49,7 +54,6 @@ function App() {
           <TopPage
             countriesJson={countriesJson}
             setCountry={setCountry}
-            getCountryData={getCountryData}
             countryData={countryData}
             loading={loading}
           />
--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -3,13 +3,13 @@ import Title from '../components/Title';
 import Selector from '../components/Selector';
 import Results from '../components/Results';
 
-const TopPage = ({countriesJson, setCountry, getCountryData, countryData, loading}) => {
+const TopPage = ({countriesJson, setCountry, countryData, loading}) => {
   return (
     <div className="top-page-container">
       <div>
         <Header />
         <Title />
-        <Selector countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} />
+        <Selector countriesJson={countriesJson} setCountry={setCountry} />
         <Results countryData={countryData} loading={loading} />
       </div>
     </div>
